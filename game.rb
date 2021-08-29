385fe8541d7258e574ff3dbb48228f1ff0428949
Archesper <pk4nde@gmail.com>
2021-08-29 23:51:53 +0100

    Refactor Game#play

--- game.rb
+++ game.rb
@@ -10,32 +10,55 @@ class Game
 
   def initialize(secret_word)
     @secret_word = secret_word
+    @correct_letters = @secret_word.split('').map(&:downcase)
     @guess_count = 0
     @correct_guessed_letters = []
     @wrong_guessed_letters = []
   end
 
   def play
-    correct_letters = @secret_word.split('').map(&:downcase)
     puts display_word_length
-    until @guess_count == 10 || (correct_letters - @correct_guessed_letters).empty?
+    until game_over? || game_solved?
       puts display_word
       puts previous_guesses unless @wrong_guessed_letters.empty?
       puts incorrect_guesses_left
-      guess = gets.chomp.downcase
-      until guess.is_alpha? && !(@wrong_guessed_letters + @correct_guessed_letters).include?(guess)
-        puts guess.is_alpha? ? already_guessed(guess) : invalid_input_message
-        guess = gets.chomp.downcase
-      end
-      if correct_letters.include?(guess)
-        puts correct_guess_message(guess)
-        @correct_guessed_letters.push(guess)
-      else
-        puts wrong_guess_message(guess)
-        @guess_count += 1
-        @wrong_guessed_letters.push(guess)
-      end
+      guess = player_guess
+      update_guessed_letters(guess)
     end
     puts @guess_count == 10 ? failure_message : victory_message
+  end
+
+  private
+
+  def player_guess
+    guess = gets.chomp.downcase
+    until guess.is_alpha? && not_guessed?(guess)
+      puts guess.is_alpha? ? already_guessed(guess) : invalid_input_message
+      guess = gets.chomp.downcase
+    end
+    guess
+  end
+
+  def update_guessed_letters(guess)
+    if @correct_letters.include?(guess)
+      puts correct_guess_message(guess)
+      @correct_guessed_letters.push(guess)
+    else
+      puts wrong_guess_message(guess)
+      @guess_count += 1
+      @wrong_guessed_letters.push(guess)
+    end
+  end
+
+  def not_guessed?(guess)
+    !(@wrong_guessed_letters + @correct_guessed_letters).include?(guess)
+  end
+
+  def game_over?
+    @guess_count == 10
+  end
+
+  def game_solved?
+    (@correct_letters - @correct_guessed_letters).empty?
   end
 end
