e17a46c938d7e7e626ccd7a64ecc8d429a633124
Archesper <pk4nde@gmail.com>
2021-08-30 18:57:13 +0100

    Fix empty string crashing #is_alpha? method

--- helper.rb
+++ helper.rb
@@ -1,5 +1,7 @@
 class String
   def is_alpha?
-    self.downcase.ord.between?(97, 122) && self.length == 1
+    return false if empty?
+  
+    downcase.ord.between?(97, 122) && length == 1
   end
 end
